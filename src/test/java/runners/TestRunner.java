afa14c01ea89a1880cf7c251c454694acd49c384
MurshidMac <mh.murshidco.uk@outlook.com>
2026-06-23 00:24:42 +0400

    Cluecumber reports added

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "pretty",
                 "html:target/cucumber-html-report.html",
+                "json:target/cucumber-reports/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         monochrome = true
